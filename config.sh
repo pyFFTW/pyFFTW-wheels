e6c49ecabc9773ef354820cc9f6eddd81af83644
Alex Rothberg <agrothberg@gmail.com>
2018-03-01 00:11:14 -0500

    clean cflags

--- config.sh
+++ config.sh
@@ -3,9 +3,6 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
 
-    # https://github.com/matthew-brett/multibuild/pull/146
-    yum install -y rsync
-
     # Taken from: https://github.com/conda-forge/fftw-feedstock/blob/master/recipe/build.sh
     export CFLAGS="-O3 -fomit-frame-pointer -fstrict-aliasing -ffast-math"
 
@@ -45,6 +42,9 @@ function pre_build {
     # https://github.com/conda-forge/fftw-feedstock/blob/8eaa8a1c63e7fcb97c63c1ee8e33c62ef3afa9c7/recipe/meta.yaml#L29-L52
     ls -l $C_INCLUDE_PATH/fftw3*
     ls -l $STATIC_FFTW_DIR/libfftw3*
+
+    # Clear CFLAGS from fftw build
+    export CFLAGS=""
 
     if [[ `uname` == 'Linux' ]]; then
         # -Bsymbolic link flag to ensure MKL FFT routines don't shadow FFTW ones.
