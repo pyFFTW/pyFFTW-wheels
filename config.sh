2093e1e4e9b719264d3705e490dfdfe792019aa2
Alex Rothberg <agrothberg@gmail.com>
2018-02-28 23:39:24 -0500

    log our dir contents

--- config.sh
+++ config.sh
@@ -21,6 +21,17 @@ function pre_build {
 
     # define STATIC_FFTW_DIR so the patched setup.py will statically link FFTW
     export STATIC_FFTW_DIR=$BUILD_PREFIX/lib
+
+    # TODO: These can be made into asserts per:
+    # https://github.com/conda-forge/fftw-feedstock/blob/8eaa8a1c63e7fcb97c63c1ee8e33c62ef3afa9c7/recipe/meta.yaml#L29-L52
+    ls -l $C_INCLUDE_PATH
+    ls -l $STATIC_FFTW_DIR
+
+    if [[ `uname` == 'Linux' ]]; then
+        # -Bsymbolic link flag to ensure MKL FFT routines don't shadow FFTW ones.
+        # see:  https://github.com/pyFFTW/pyFFTW/issues/40
+        export CFLAGS="$CFLAGS -Wl,-Bsymbolic"
+    fi
 }
 
 function run_tests {
